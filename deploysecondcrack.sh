513d96f5dd4eaec235444e4a5b3ead26fbaaad5e
zmartell <zak.martell@gmail.com>
2013-09-28 11:54:05 -0400

    Update deploysecondcrack.sh
    cleaned up code

--- deploysecondcrack.sh
+++ deploysecondcrack.sh
@@ -2,28 +2,18 @@
 
 sudo yum install -y httpd php php-process python git gcc automake autoconf libtool make curl-devel libgcc.i686 glibc.i686 php-xml php-mbstring;
 
-# Start Apache on boot and turn off firewall
+# Start Apache on boot 
 
 sudo chkconfig  httpd on;
-sudo chkconfig iptables off;
 
 # Install inotify-tools to make updating speedy
 
-wget --no-check-certificate http://github.com/downloads/rvoicilas/inotify-tools/inotify-tools-3.14.tar.gz; 
-  tar xvfz inotify-tools-3.14.tar.gz;
+wget --no-check-certificate http://github.com/downloads/rvoicilas/inotify-tools/inotify-tools-3.14.tar.gz | tar xvfz -;
   cd inotify-tools-3.14;
   ./configure --prefix=/usr --libdir=/lib64;
   sudo make;
   sudo make install;
   cd ~ ; sudo rm inotify-tools-3.14.tar.gz;
-
-# Install start-stop-daemon for use in /etc/init.d/dropbox
-
-cd ~ ; wget http://developer.axis.com/download/distribution/apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz;
-  tar zxvf apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz;
-  cd apps/sys-utils/start-stop-daemon-IR1_9_18-2/;
-  sudo gcc start-stop-daemon.c -o start-stop-daemon;
-  cd ~ ; sudo rm apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz
 
 # Install deploysecondcrack
 
@@ -43,7 +33,7 @@ sudo chmod -R o+x  ~/Dropbox;
 
 # Install Dropbox service
 
-cd ~; sudo cp ~/deploysecondcrack/config-files/dropbox-service /etc/init.d/dropbox;
+sudo cp ~/deploysecondcrack/config-files/dropbox-service /etc/init.d/dropbox;
 sudo cp ~/deploysecondcrack/config-files/sysconfig-dropbox /etc/sysconfig/dropbox;
 sudo chmod 755 /etc/init.d/dropbox
 sudo chkconfig --add dropbox;
@@ -62,7 +52,6 @@ sudo crontab ~/deploysecondcrack/config-files/crontab.example;
 # Config Apache with default settings
 
 sudo cp ~/deploysecondcrack/config-files/httpd.conf /etc/httpd/conf/httpd.conf;
-sudo service iptables stop;
 sudo rm /etc/httpd/conf.d/welcome.conf;
 sudo chmod o+x ~;
 
